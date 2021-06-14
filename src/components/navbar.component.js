ef351ddfbe757a99741f46d6c04e8167cd463b13
rana1998 <2015Kshitij14@gmail.com>
2021-06-14 09:34:50 +0530

    fff

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -9,17 +9,17 @@ export default class Navbar extends Component {
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
-                            <Link to="/" classNaame="nav-link">
+                            <Link to="/" className="nav-link">
                                 Exercises
                             </Link>                       
                         </li>
                         <li className="navbar-item">
-                            <Link to="/create" classNaame="nav-link">
+                            <Link to="/create" className="nav-link">
                                 Create Exercise Log
                             </Link>                       
                         </li>
                         <li className="navbar-item">
-                            <Link to="/user" classNaame="nav-link">
+                            <Link to="/user" className="nav-link">
                                 Create User
                             </Link>                       
                         </li>
