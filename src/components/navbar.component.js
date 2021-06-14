724455e67806e41b6d0223f1305c3bd6c566c74e
rana1998 <2015Kshitij14@gmail.com>
2021-06-14 09:24:38 +0530

    CreateExercise

---
+++ src/components/navbar.component.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to='/' className="navbar-brand">ExcerTracker</Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/" classNaame="nav-link">
+                                Exercises
+                            </Link>                       
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" classNaame="nav-link">
+                                Create Exercise Log
+                            </Link>                       
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/user" classNaame="nav-link">
+                                Create User
+                            </Link>                       
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
